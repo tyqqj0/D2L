2ae484508e5d8746c12fefa3a1fcf1a0a82c6172
tyqqj0 <tyqqj0@163.com>
2024-03-27 19:08:34 +0800

    观察模型表现

--- model/resnet18.py
+++ model/resnet18.py
@@ -7,20 +7,41 @@
 import torch
 from torch import nn as nn
 from torchvision import models
-from torch import flatten # 对卷积是否正确存疑 TODO
-
+from torch import flatten  # 对卷积是否正确存疑 TODO
 
 __ALL__ = ['ResNet18FeatureExtractor']
+
+from torchvision.models import ResNet18_Weights
 
 
 class ResNet18FeatureExtractor(nn.Module):
     def __init__(self, pretrained=False, num_classes=10, in_channels=1):
         super(ResNet18FeatureExtractor, self).__init__()
         # 使用ResNet18模型，设置输入通道数为1，输出类别数为10
-        self.resnet18 = models.resnet18(pretrained=pretrained, num_classes=num_classes)
-        # 修改第一个卷积层为单通道输入
+        if pretrained:
+            # 使用默认的预训练权重
+            weights = ResNet18_Weights.DEFAULT
+        else:
+            # 如果不使用预训练模型，则权重为None
+            weights = None
+
+            # 加载ResNet18模型
+        self.resnet18 = models.resnet18(weights=weights, num_classes=num_classes)
+
+        # 如果输入通道数不是3，需要修改模型的第一层
         if in_channels != 3:
-            self.resnet18.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+            # 获取原始第一层的权重
+            original_first_layer = self.resnet18.conv1
+            # 创建一个新的第一层，输入通道数为in_channels
+            new_first_layer = nn.Conv2d(in_channels, original_first_layer.out_channels,
+                                        kernel_size=original_first_layer.kernel_size,
+                                        stride=original_first_layer.stride,
+                                        padding=original_first_layer.padding,
+                                        bias=original_first_layer.bias)
+
+            # 如果是使用预训练权重的话，这里可以复制权重，取平均等方式修改权重
+            # 但是这里只是简单地用随机权重初始化
+            self.resnet18.conv1 = new_first_layer
 
         # 如果你需要使用预训练权重，你可能需要手动复制权重从3通道到1通道
         # 这里是一个简单的平均权重的方法，如果你有预训练的权重，那么取消下面两行的注释
@@ -33,7 +54,7 @@ class ResNet18FeatureExtractor(nn.Module):
         # Use ResNet18 up to the second-to-last layer to get the features
         features = {}
         x = self.resnet18.conv1(x)
-        features['conv1'] = flatten(x, 1)# (16, 64, 14, 14) (batch_size, C, H, W)->(batch_size, C*H*W)
+        features['conv1'] = flatten(x, 1)  # (16, 64, 14, 14) (batch_size, C, H, W)->(batch_size, C*H*W)
         x = self.resnet18.bn1(x)
         features['bn1'] = flatten(x, 1)
         x = self.resnet18.relu(x)
--- model/resnet50.py
+++ model/resnet50.py
@@ -10,16 +10,38 @@ from torch import nn as nn
 from torchvision import models
 from torch import flatten  # 对卷积是否正确存疑 TODO
 
-
 __ALL__ = ['ResNet50FeatureExtractor']
+
+from torchvision.models import ResNet50_Weights
 
 
 class ResNet50FeatureExtractor(nn.Module):
     def __init__(self, pretrained=False, num_classes=10, in_channels=1):
         super(ResNet50FeatureExtractor, self).__init__()
-        self.resnet50 = models.resnet50(pretrained=pretrained, num_classes=num_classes)
+        if pretrained:
+            # 使用默认的预训练权重
+            weights = ResNet50_Weights.DEFAULT
+        else:
+            # 如果不使用预训练模型，则权重为None
+            weights = None
+
+            # 加载ResNet18模型
+        self.resnet50 = models.resnet50(weights=weights, num_classes=num_classes)
+
+        # 如果输入通道数不是3，需要修改模型的第一层
         if in_channels != 3:
-            self.resnet50.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+            # 获取原始第一层的权重
+            original_first_layer = self.resnet50.conv1
+            # 创建一个新的第一层，输入通道数为in_channels
+            new_first_layer = nn.Conv2d(in_channels, original_first_layer.out_channels,
+                                        kernel_size=original_first_layer.kernel_size,
+                                        stride=original_first_layer.stride,
+                                        padding=original_first_layer.padding,
+                                        bias=original_first_layer.bias)
+
+            # 如果是使用预训练权重的话，这里可以复制权重，取平均等方式修改权重
+            # 但是这里只是简单地用随机权重初始化
+            self.resnet50.conv1 = new_first_layer
 
         if pretrained:
             conv1_weight = self.resnet50.conv1.weight.data.mean(dim=1, keepdim=True)
